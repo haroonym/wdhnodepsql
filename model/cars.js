5415fad84df585c09b773a472937a5d47cf39b50
unknown <younas.h03@htlwienwest.at>
2021-09-29 13:35:34 +0200

    DELETE verbessert

--- model/cars.js
+++ model/cars.js
@@ -12,11 +12,12 @@ async function delCarsRoutes(id) {
   const { rows } = await db.query('SELECT * FROM cars where id = $1', [id]);
   if (rows.length > 0) {
     await db.query('DELETE FROM cars WHERE id = $1', [id]);
+    return {
+      code: 200,
+      data: true,
+    };
   }
-  return {
-    code: 200,
-    data: true,
-  };
+  return { code: 404, data: `id ${id} was not found in the database` };
 }
 
 module.exports = {
