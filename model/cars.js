0a0a11b0dedb4c74d104a337c2cb369e1c11dafa
unknown <younas.h03@htlwienwest.at>
2021-10-06 20:18:47 +0200

    Delete geupdated und Insert fertig

--- model/cars.js
+++ model/cars.js
@@ -11,25 +11,25 @@ async function getCarsRoutes() {
 }
 
 async function delCarsRoutes(id) {
-  const { rows } = await db.query('SELECT * FROM cars where id = $1', [id]);
-  if (rows.length > 0) {
-    await db.query('DELETE FROM cars WHERE id = $1', [id]);
-    return {
-      code: 200,
-      data: true,
-    };
+  const { rows } = await db.query('DELETE FROM cars WHERE id = $1', [id]);
+  if (rows.length < 0) {
+    return { code: 404, data: `id ${id} was not found in the database` };
   }
-  return { code: 404, data: `id ${id} was not found in the database` };
+  return {
+    code: 200,
+    data: `Car with the id ${id} has been deleted`,
+  };
 }
 
 async function patchCarsRoutes(id, data) {
-  const result = await db.query('SELECT * FROM cars where id = $1', [id]);
-  if (result.code != 200) return result;
+  const { result } = await db.query('SELECT * FROM cars WHERE id = $1', [id]);
+  if (result.code != 200) return { code: 404, data: `id ${id} was not found in the database` };
+
   const props = [];
   for (const prop in data) {
     props.push(`${prop} = '${data[prop]}'`);
   }
-  const cmd = `UPDATE employees SET ${props.join(',')} WHERE employee_id = $1`;
+  const cmd = `UPDATE cars SET ${props.join(',')} WHERE id = $1`;
   await db.query(cmd, [id]);
   return {
     code: 200,
@@ -37,17 +37,27 @@ async function patchCarsRoutes(id, data) {
   };
 }
 
+async function getIdFromOwner(owner) {
+  const { rows } = await db.query('SELECT id FROM owner WHERE first_name = $1 AND last_name = $2', [
+    owner.firstName,
+    owner.lastName,
+  ]);
+  return rows[0].id;
+}
+
 async function insertCarRoutes(c) {
   const { rows } = await db.query('SELECT MAX(id) AS max FROM cars');
   const id = rows[0].max + 1;
+  const idOwn = await getIdFromOwner(c.owner);
+
   await db.query(
     `INSERT INTO cars (id, title, image, status, price, miles, year_of_make, description, owner)
                            VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)`,
-    [id, c.title, c.image, c.status, c.price, c.miles, c.year_of_make, c.description, c.owner],
+    [id, c.title, c.image, c.status, c.price, c.miles, c.year_of_make, c.description, idOwn],
   );
   return {
     code: 200,
-    data: id,
+    data: `Car with ID: ${id} has been added`,
   };
 }
 
